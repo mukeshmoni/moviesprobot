06c7fd17c47bc1354dcfe11702f7d17dbb461177
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-07 19:34:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,38 +13,38 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', "24274375"))
+API_HASH = environ.get('API_HASH', "9d88affc9ab571a6695e069cef5d363b")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7963986526:AAHW-8ecZU3oqc5U7kAb9VDD9sw0P2AguO8")
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5665677555').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "7290811275").split()]
+USERNAME = environ.get('USERNAME', "https://t.me/KingofAtttitude") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001770039008'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/CinemaMovie_Group')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001770039008').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbots.5yddr.mongodb.net/?retryWrites=true&w=majority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vkyvk9515:vkyvk9515@cluster0.3z7tbya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "BillaMovie_bot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', '( @𝐼𝑡𝑧_𝑚𝑜𝑣𝑖𝑒_𝑡𝑖𝑚𝑒_2𝑘 ) 𝑓𝑎𝑚𝑖𝑙𝑦')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))  
-BIN_CHANNEL = int(environ.get('BIN_CHANNEL','0'))
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
-auth_channel = environ.get('AUTH_CHANNEL', '')
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0')
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', 'https://t.me/CinemaMovies_S'))  
+BIN_CHANNEL = int(environ.get('BIN_CHANNEL','https://t.me/CinemaMovies_S'))
+DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','https://t.me/CinemaMovies_S'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', "https://t.me/CinemaMovies_S"))
+auth_channel = environ.get('AUTH_CHANNEL', 'https://t.me/CinemaMovies_S')
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 'https://t.me/CinemaMovie_Group'))
+request_channel = environ.get('REQUEST_CHANNEL', 'https://t.me/CinemaMovies_S')
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', 'https://t.me/CinemaMovies_S'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/CinemaMovie_Group') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/CinemaMovie_Group")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
